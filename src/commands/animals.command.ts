1620eaca2e2541e76771148ab990090cf2c25376
LizavetaLeiko <lizaleiko3@gmail.com>
2023-05-30 08:21:56 +0300

    feat button for getting one more animal

--- src/commands/animals.command.ts
+++ src/commands/animals.command.ts
@@ -1,4 +1,4 @@
-import { Telegraf } from "telegraf";
+import { Markup, Telegraf } from "telegraf";
 import { Command } from "./command.class";
 import { IBotContext } from "../context/context.interface";
 import axios, { AxiosResponse } from "axios";
@@ -15,13 +15,19 @@ export class AnimalCommand extends Command{
   }
   handle(): void {
     this.bot.command(this.animal , async ctx => {
-      let randomPage = Math.floor(Math.random() * 50)
+      this.getQuery(ctx)
+    })
+    this.bot.action('more', ctx =>{
+      this.getQuery(ctx)
+    })
+  }
+  async getQuery(ctx: IBotContext): Promise<any>{
+    let randomPage = Math.floor(Math.random() * 50)
       try {
         const response: AxiosResponse<any> = await axios.get<any>(`https://api.pexels.com/v1/search?query=${this.animal}&per_page=1&page=${randomPage}`, {headers: {Authorization: this.configService.get('PICTURES_TOKEN')}});
-        ctx.replyWithPhoto(response.data.photos[0].url ? response.data.photos[0].url : 'sorry, something is wrong with photo, try again')
+        ctx.replyWithPhoto(response.data.photos[0].url ? response.data.photos[0].url : 'sorry, something is wrong with photo, try again', Markup.inlineKeyboard([Markup.button.callback('One more photo', 'more')]))
       } catch (err) {
         ctx.reply(`Sorry, something is wrong`)
       }
-    })
   }
 }
--- src/commands/weather.command.ts
+++ src/commands/weather.command.ts
@@ -1,34 +1,53 @@
-import { Telegraf } from "telegraf";
+import { Markup, Telegraf } from "telegraf";
 import { Command } from "./command.class";
 import { IBotContext } from "../context/context.interface";
-import axios, { AxiosResponse } from "axios"
+import axios, { AxiosResponse } from "axios";
 import { IConfigService } from "../config/config.interface";
 
-
-export class WeatherCommand extends Command{
+export class WeatherCommand extends Command {
   configService: IConfigService;
-  constructor(public bot: Telegraf<IBotContext>, configService: IConfigService){
-    super(bot)
-    this.configService = configService
+  constructor(public bot: Telegraf<IBotContext>,configService: IConfigService ) {
+    super(bot);
+    this.configService = configService;
   }
   handle(): void {
-    this.bot.command('weater', async ctx => {
-      ctx.sendMessage('What is your city?')
+    this.bot.command("weater", async (ctx) => {
+      ctx.sendMessage("What is your city?");
       this.bot.hears(/.*/, async (ctx2) => {
-        let city: string = ctx2.message.text;
-        if (city) {
-          try {
-            const response: AxiosResponse<any> = await axios.get<any>(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.configService.get('WEATHER_TOKEN')}`);
-            await ctx2.reply(
-  `Weather in ${city}:
-  temperature: ${response.data.main.temp ? Math.floor(response.data.main.temp - 273) : 'there is no information'},
-  sky: ${response.data.weather[0].description ? response.data.weather[0].description : 'there is no information' },
-  wind: ${response.data.wind.speed ? response.data.wind.speed : 'there is no information'} `)
-          } catch (err) {
-            await ctx2.reply(`The weather in ${city} is not found. Please, check your town (it should be in english and without any spaces, smileys, quotes, etc.) or try later`)
-          }
-        }
+        this.getQuery(ctx2)
       });
-    })
+    });
+  }
+  async getQuery(ctx: any): Promise<void> {
+    let city: string = ctx.message.text;
+    if (city) {
+      try {
+        const response: AxiosResponse<any> = await axios.get<any>(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.configService.get(
+            "WEATHER_TOKEN"
+          )}`
+        );
+        await ctx.reply(
+          `Weather in ${city}:
+  temperature: ${
+    response.data.main.temp
+      ? Math.floor(response.data.main.temp - 273)
+      : "there is no information"
+  },
+  sky: ${
+    response.data.weather[0].description
+      ? response.data.weather[0].description
+      : "there is no information"
+  },
+  wind: ${
+    response.data.wind.speed
+      ? response.data.wind.speed
+      : "there is no information"
+  } `)} catch (err) {
+        await ctx.reply(
+          `The weather in ${city} is not found. Please, check your town (it should be in english and without any spaces, smileys, quotes, etc.) or try later`
+        );
+      }
+    }
   }
 }
